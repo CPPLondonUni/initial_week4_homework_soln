b1f9e49ebf472bf3f6769c434059e404b05ba4a1
Tristan Brindle <t.c.brindle@gmail.com>
2018-10-16 17:18:33 +0100

    Add exercise 2 solution

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <limits>
+#include <vector>
 
-int main()
+double get_min(std::vector<double> vec)
 {
-    std::cout << "Enter some numbers:" << std::endl;
-    
-    double min = std::numeric_limits<double>::max();
-    double max = std::numeric_limits<double>::lowest();
-    double d = 0;
-    
-    while (std::cin >> d) {
+    auto min = std::numeric_limits<double>::max();
+    for (auto d : vec) {
         if (d < min) {
             min = d;
         }
+    }
+    return min;
+}
+
+double get_max(std::vector<double> vec)
+{
+    auto max = std::numeric_limits<double>::lowest();
+    for (auto d : vec) {
         if (d > max) {
             max = d;
         }
     }
+    return max;
+}
+
+double get_mean(std::vector<double> vec)
+{
+    double sum = 0.0;
+    for (auto d : vec) {
+        sum += d;
+    }
+    return sum/vec.size();
+}
+
+int main()
+{
+    std::cout << "Enter some numbers:" << std::endl;
+
+    double d;
+    std::vector<double> values;
+
+    while (std::cin >> d) {
+        values.push_back(d);
+    }
     
-    std::cout << "Minimum was " << min << '\n';
-    std::cout << "Maximum was " << max << '\n';
+    std::cout << "Minimum was " << get_min(values) << '\n';
+    std::cout << "Maximum was " << get_max(values) << '\n';
+    std::cout << "Mean was " << get_mean(values) << '\n';
 }
