d66cc74b97a4e50d05133034a84bc818b0a62051
Tristan Brindle <t.c.brindle@gmail.com>
2018-10-16 17:14:16 +0100

    Add exercise 1 solution

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << "Enter some numbers:" << std::endl;
+    
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::min();
+    double d = 0;
+    
+    while (std::cin >> d) {
+        if (d < min) {
+            min = d;
+        }
+        if (d > max) {
+            max = d;
+        }
+    }
+    
+    std::cout << "Minimum was " << min << '\n';
+    std::cout << "Maximum was " << max << '\n';
+}
